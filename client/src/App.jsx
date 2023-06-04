f5370222a1c9c030d6790ef9437fefed155338a4
rkshawon <rkshawon16@gmail.com>
2023-06-04 17:21:10 +0600

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl text-gray-600 font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
